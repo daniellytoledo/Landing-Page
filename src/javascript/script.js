a7c2db9cf135551877fb71f6d41346a8ccc92bd3
Dani <daniellytoledosouto@gmail.com>
2025-03-22 11:10:09 +0100

    colors changes

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#mobile_btn').on('click', function () {
+        console.log('Botão móvel clicado!'); // Verifique no console do navegador
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x'); 
     });
 });
-
